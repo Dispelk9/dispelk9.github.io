ed1338e07bf9a76aacbd0e398320c2d75f128c5a
Ai Viet Hoang <extern.ai_viet.hoang@porsche.de>
2025-07-09 11:23:02 +0200

    fix navbar dynamic

--- js/loadcomponents.js
+++ js/loadcomponents.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Load Head
-    fetch("head.html")
-        .then(response => response.text())
-        .then(html => {
+    fetch("head.html").then(response => response.text()).then(html => {
             document.head.innerHTML += html;
-        });
+    });
 
-    // Load Navbar
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("navbar-container").innerHTML = html;
+    fetch("navbar.html").then(res => res.text()).then(html => {
+        document.getElementById("navbar-container").innerHTML = html;
+
+        // Re-initialize dropdowns (Bootstrap 5 does this automatically only on load)
+        var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+        dropdownElementList.map(function (dropdownToggleEl) {
+            new bootstrap.Dropdown(dropdownToggleEl);
         });
+    });
 });
